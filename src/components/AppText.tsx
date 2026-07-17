86549ad2fa7b912d6ddfdfa4c902651f2f6f7086
Mani <itsamanihd@gmail.com>
2026-07-17 16:18:20 +0100

    Created modular TextStyle for typography design

---
+++ src/components/AppText.tsx
@@ -0,0 +1,39 @@
+// --- IMPORTS ---
+import { Text } from 'react-native';
+import { processFontWeight } from 'react-native-reanimated/lib/typescript/common';
+
+// --- TYPES ---
+type AppTextProps = {
+    variant?: 'title' | 'h1' | 'subtitle' | 'body';
+    children: string; // Whatever is placed inside the tags
+};
+
+// --- THE COMPONENT ---
+// Set varient = 'body' to set a default
+export default function AppText({ variant = 'body', children }: AppTextProps) {
+
+    let textStyle = {}; // Empty style object
+
+    switch (variant) {
+        case 'title':
+            textStyle = { fontSize: 32, fontWeight: 'bold', color: '#5C4033', marginBottom: 15 };
+            break;
+        case 'h1':
+            textStyle = { fontSize: 24, fontWeight: 'bold', color: '#A06B42', marginBottom: 10 };
+            break;
+        case 'subtitle':
+            textStyle = { fontSize: 18, fontWeight: '600', color: '#D4A373', marginBottom: 5 };
+            break;
+        case 'body':
+        default:
+            textStyle = { fontSize: 14, color: '#333333'};
+            break;
+    }
+
+    // Pass chosen textStyle into standard <Text> tag
+    return (
+        <Text style={textStyle}>
+            {children}
+        </Text>
+    );
+}
